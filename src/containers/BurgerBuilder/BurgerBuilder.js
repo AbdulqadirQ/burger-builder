ec72de89b8967c380712cc8d0118443f75b97f09
Abdulqadir Qasim <a.q.r@hotmail.co.uk>
2019-05-06 19:03:20 +0100

    add functionality to display burger ingredients dynamicly based on state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary";
-import Burger from '../../components/Burger/Burger'
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      beef: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
+
   render() {
     return (
       <Auxiliary>
-          <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Auxiliary>
     );
